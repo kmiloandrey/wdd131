ac898bd6cae416096d0cec2f44d9e6b444365516
kmiloandrey <andreycamilo.kal@gmail.com>
2026-08-30 21:29:46 -0600

    update index

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// year
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// last modified time
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
