e3876da41b8b866cb71203701c2eb09f83740867
kmiloandrey <andreycamilo.kal@gmail.com>
2026-09-09 14:51:50 -0600

    place

---
+++ scripts/place.js
@@ -0,0 +1,4 @@
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+/* last modified time for footer */
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
